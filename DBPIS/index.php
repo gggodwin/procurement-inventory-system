0d79db8378b6936beba7fcd1714a26e76f0efdcd
GNS-Dango <gnsantiago.tech@gmail.com>
2024-09-17 19:57:34 +0800

    added a toast

--- DBPIS/index.php
+++ DBPIS/index.php
@@ -1,4 +1,3 @@
-
 <?php 
 include ("misc/header.php"); 
 include ("core/dbsys.ini");
@@ -7,13 +6,15 @@ include_once ("query/system.qry");
 session_start();
 
 $system = new SYSTEM();
+$invalid_login = false; // Initialize the flag for invalid login
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['username']) && !empty($_POST['password'])) {
     if ($system->get_validateuser($db, $_POST["username"], $_POST["password"])) {
         header("Location: admin/sys_admin/main_admin.php");
         exit(); // Ensure no further code execution after redirect
+    } else {
+        $invalid_login = true; // Set the flag to true if login fails
     }
-    echo "Invalid username or password."; // Optional: Handle invalid credentials
 }
 ?>
 
@@ -33,8 +34,35 @@ $(document).ready(function() {
             console.error('AJAX Error:', status, error);
         }
     });
+
+    // Display a toast if login is invalid
+    <?php if ($invalid_login): ?>
+        showToast("Invalid username or password. Please try again.");
+    <?php endif; ?>
 });
+
+// Custom toast function
+function showToast(message) {
+    var toast = document.createElement('div');
+    toast.className = 'custom-toast';
+    toast.innerText = message;
+    
+    document.body.appendChild(toast);
+
+    setTimeout(function() {
+        toast.classList.add('visible');
+    }, 100); // Slight delay to allow transition
+
+    setTimeout(function() {
+        toast.classList.remove('visible');
+        setTimeout(function() {
+            document.body.removeChild(toast);
+        }, 300); // Delay to remove the element after the fade-out
+    }, 3000); // Display for 3 seconds
+}
 </script>
-<div id="loginContainer" class ="bg-black">
-        <!-- The PHP content will be loaded here -->
+
+
+<div id="loginContainer" class="bg-black">
+    <!-- The PHP content will be loaded here -->
 </div>
---
+++ DBPIS/misc/footer.php
@@ -0,0 +1,19 @@
+                    <script src="plugins/jquery/jquery.min.js"></script>
+                    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+                    <script src="plugins/simplebar/simplebar.min.js"></script>
+                    <script src="https://unpkg.com/hotkeys-js/dist/hotkeys.min.js"></script>
+
+                    
+                    
+                    <script src="plugins/prism/prism.js"></script>
+                    
+                    
+                    
+                    <script src="plugins/toaster/toastr.min.js"></script>
+
+                    
+                    
+                    <script src="js/mono.js"></script>
+                    <script src="js/chart.js"></script>
+                    <script src="js/map.js"></script>
+                    <script src="js/custom.js"></script>
