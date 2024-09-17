33ff676dba41ffaf4104039d6a0dc9ec2d7303b9
GNS-Dango <gnsantiago.tech@gmail.com>
2024-09-17 21:05:38 +0800

    main

--- DBPIS/admin/sys_admin/main_admin.php
+++ DBPIS/admin/sys_admin/main_admin.php
@@ -1,21 +1,9 @@
 
 <?php 
-include ("../../misc/page_loader.php"); 
 include ("../../misc/header_main.php");
 include ("../../misc/sidebar.php");
 include ("../../misc/navbar.php");
 include ("../../misc/footer_main.php");
 ?>
-
-
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="../../../css/page_loader.css" />
-    <script>
-        $(document).ready(function() {
-            setTimeout(function() {
-                $(".loader-wrapper").fadeOut("slow");
-            }, 800); // 2000 milliseconds = 2 seconds
-        });
-    </script>
 
 
