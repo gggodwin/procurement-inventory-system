c829407225f7c0c2f5773377481e0e6ac9f64c48
GNS-Dango <gnsantiago.tech@gmail.com>
2024-09-20 19:27:03 +0800

    updated  sql

--- dbpis.sql
+++ dbpis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2024 at 12:55 PM
+-- Generation Time: Sep 20, 2024 at 01:26 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -21,6 +21,39 @@ SET time_zone = "+00:00";
 --
 -- Database: `dbpis`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dbpis_items`
+--
+
+CREATE TABLE `dbpis_items` (
+  `id` int(11) NOT NULL,
+  `barcode` varchar(255) DEFAULT NULL,
+  `particular` varchar(255) DEFAULT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `category` varchar(100) DEFAULT NULL,
+  `safety_stock` int(11) DEFAULT NULL,
+  `current_stock` int(11) DEFAULT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dbpis_items`
+--
+
+INSERT INTO `dbpis_items` (`id`, `barcode`, `particular`, `brand`, `category`, `safety_stock`, `current_stock`, `last_updated`) VALUES
+(1, '100000000001', 'Hydraulic Pump', 'Bosch', 'Mechanical', 20, 100, '2024-09-20 09:39:16'),
+(2, '100000000002', 'Alternator', 'Delphi', 'Automotive', 10, 50, '2024-09-20 09:39:16'),
+(3, '100000000003', 'Servo Motor', 'Siemens', 'Electromechanical', 15, 80, '2024-09-20 09:39:16'),
+(4, '100000000004', 'Transmission Belt', 'Gates', 'Mechanical', 25, 120, '2024-09-20 09:39:16'),
+(5, '100000000005', 'Carburetor', 'Holley', 'Automotive', 5, 30, '2024-09-20 09:39:16'),
+(6, '100000000006', 'Electric Motor', 'ABB', 'Electromechanical', 10, 40, '2024-09-20 09:39:16'),
+(7, '100000000007', 'Gearbox', 'SEW-Eurodrive', 'Mechanical', 8, 25, '2024-09-20 09:39:16'),
+(8, '100000000008', 'RJ-45', 'Phoenix', 'Information Tech', 50, 60, '2024-09-20 09:39:16'),
+(9, '100000000009', 'Relay Switch', 'Schneider', 'Electromechanical', 30, 150, '2024-09-20 09:39:16'),
+(10, '100000000010', 'Fuel Pump', 'Denso', 'Automotive', 6, 35, '2024-09-20 09:39:16');
 
 -- --------------------------------------------------------
 
@@ -50,6 +83,12 @@ INSERT INTO `dbpis_useraccounts` (`uaid`, `name`, `username`, `password`, `statu
 --
 
 --
+-- Indexes for table `dbpis_items`
+--
+ALTER TABLE `dbpis_items`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `dbpis_useraccounts`
 --
 ALTER TABLE `dbpis_useraccounts`
@@ -58,6 +97,12 @@ ALTER TABLE `dbpis_useraccounts`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `dbpis_items`
+--
+ALTER TABLE `dbpis_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `dbpis_useraccounts`
